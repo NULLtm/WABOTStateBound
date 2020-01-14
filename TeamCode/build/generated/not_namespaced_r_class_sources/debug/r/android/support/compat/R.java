3029d17d081224632d4eb0a45c8f8143064cb879
NULLtm <boseleyowe@tarriers.org>
2020-01-13 16:03:43 -0800

    cc
    dd

--- TeamCode/build/generated/not_namespaced_r_class_sources/debug/r/android/support/compat/R.java
+++ TeamCode/build/generated/not_namespaced_r_class_sources/debug/r/android/support/compat/R.java
@@ -28,10 +28,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f03003d;
-        public static final int notification_icon_bg_color = 0x7f03003e;
-        public static final int ripple_material_light = 0x7f03003f;
-        public static final int secondary_text_default_material_light = 0x7f030040;
+        public static final int notification_action_color_filter = 0x7f03003e;
+        public static final int notification_icon_bg_color = 0x7f03003f;
+        public static final int ripple_material_light = 0x7f030040;
+        public static final int secondary_text_default_material_light = 0x7f030041;
     }
     public static final class dimen {
         private dimen() {}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/official/OpenCVLoader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/official/OpenCVLoader.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.official;
 
-import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
+//import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
